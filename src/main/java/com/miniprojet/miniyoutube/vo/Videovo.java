c8e03bff95e6e0330d7e4cd1f6f1fed70f9e70a6
SLIM <ayyoub.boussoui@edu.uca.ma>
2021-03-13 16:13:02 +0100

    do your search method guys.

---
+++ src/main/java/com/miniprojet/miniyoutube/vo/Videovo.java
@@ -0,0 +1,31 @@
+package com.miniprojet.miniyoutube.vo;
+
+public class Videovo {
+private String ref;
+private Double totalmax;
+private Double totalmin;
+
+    public String getRef() {
+        return ref;
+    }
+
+    public Double getTotalmin() {
+        return totalmin;
+    }
+
+    public void setTotalmin(Double totalmin) {
+        this.totalmin = totalmin;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public Double getTotalmax() {
+        return totalmax;
+    }
+
+    public void setTotalmax(Double totalmax) {
+        this.totalmax = totalmax;
+    }
+}
